91d1fc4cce4c57d2efb4a28311740d16c3fc16fd
Kaj-Michael Lang <milang@tal.org>
2018-07-02 13:07:45 +0000

    Add tick, check Pi firmware throttling

--- loadtemp.c
+++ loadtemp.c
@@ -2,6 +2,22 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#define FIRMWARE_THROTTLED "/sys/devices/platform/soc/soc:firmware/get_throttled"
+
+int read_int(const char *file)
+{
+FILE *f;
+int r,tmp;
+
+f=fopen(file, "r");
+r=fscanf(f, "%d", &tmp);
+if (r==0)
+	return -1;
+fclose(f);
+
+return tmp;
+}
+
 int read_load(float *la1, float *la5, float *la15)
 {
 FILE *f;
@@ -14,6 +30,11 @@ r=fscanf(f, "%f %f %f", la1, la5, la15);
 fclose(f);
 
 return r;
+}
+
+int read_throttled()
+{
+return read_int(FIRMWARE_THROTTLED);
 }
 
 float read_temp()
@@ -37,6 +58,7 @@ void log_temp_and_load(char *log)
 {
 FILE *f;
 time_t t;
+int tick=0;
 
 f=fopen(log, "w");
 if (!f) {
@@ -44,23 +66,29 @@ if (!f) {
 	return;
 }
 
-fprintf(f, "Time,Temp,LoadAvg1,LoadAvg5,LoadAvg15\n");
+fprintf(f, "Tick,Time,Temp,LoadAvg1,LoadAvg5,LoadAvg15,Throttled\n");
 
 while (1) {
+	int th;
 	float temp,a1,a5,a15;
 
 	t=time(NULL);
 	temp=read_temp();
 	read_load(&a1, &a5, &a15);
 
-	printf("%ld,%f,%.2f,%.2f,%.2f\n", t, temp, a1, a5, a15);
+	// XXX: Check the bits
+	th=read_throttled();
 
-	int r=fprintf(f, "%ld,%.4f,%.2f,%.2f,%.2f\n", t, temp, a1, a5, a15);
+	printf("%d,%ld,%f,%.2f,%.2f,%.2f,%d\n", tick, t, temp, a1, a5, a15, th>0 ? 1 : 0);
+
+	int r=fprintf(f, "%d,%ld,%.4f,%.2f,%.2f,%.2f,%d\n", tick, t, temp, a1, a5, a15, th>0 ? 1 : 0);
 	if (r<0) {
 		perror("Failed to write to log file");
 		return;
 	}
 	fflush(f);
+
+	tick++;
 
 	sleep(1);
 }
